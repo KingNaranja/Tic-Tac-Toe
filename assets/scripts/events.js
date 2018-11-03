7bf85dbe043f72959748794031df51fb97c446ef
KingNaranja <kledaa3@gmail.com>
2018-11-03 17:00:30 -0400

    Added Game Logic - Storing Player Moves
    Added:
    - storePlayerMove() Game method
    - checkForMatch() event
    - checkWin() event
    
    Completed the logic for storing the moves of each player and placed it
    inside the Game class to use as method.
    
    Began creating the logic to resolve the game once it has begun.
    For now the checkForMatch function contains $variables of the rows and
    columns of the board. A for each loop will be used to use checkWin
    for winning combinations.
    
    -> At the moment I do not have much confidence in this approach above
      and I will play around with my logic a bit.
    -> Overall the checkForMatch function will need to be rewritten probably
    
    *For Next Commit / Current Bugs*
    - Game does not end
    - Game does not have a clear winner
    - The API still does not know what the players are doing

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -23,9 +23,9 @@ const onSignUp = event => {
 // Tic Tac Toe Engine 
 // helps handle game instances and API responses
 class Game {
-    constructor(id, cells, over, player_x={}, player_o=null) {
+    constructor(id, over, player_x={}, player_o=null) {
         this.id = id 
-        this.cells = cells 
+        this.cells = [] 
         this.over = over
         this.player_x = player_x
         this.player_o = player_o
@@ -34,7 +34,26 @@ class Game {
     // create a new game instance in the store 
     newGame() {
         let gameCount = store.currentGame.id
-        store.currentGame = new Game(`${gameCounter++}`,[],false)
+        store.currentGame = new Game(`${gameCount++}`,false)
+    }
+
+    // will grab the data of the player spot 
+    // add a player class to that element 
+    // and store the player move in the current game  
+    storePlayerMove(playerSpot) {
+        let spotClasses = playerSpot.classList
+        spotClasses.add(`${store.currentGame.turn}`)
+
+        // playerMove contains the index of the selected sqaure
+        let playerMove = playerSpot.getAttribute("data-cell")
+
+        // stores the playerMove in the current index of the currentGame cell storage
+        this.cells[playerMove] = store.currentGame.turn
+        
+        // logs the cells array in the currentGame object 
+        console.log(store.currentGame.cells)
+        
+        
     }
     
     
@@ -66,7 +85,7 @@ const makeMove = (square) =>{
 
     
     if (playerSpot.innerText == "X" || playerSpot.innerText == "O" ) { 
-        // if the player clicks on a square 2x
+        // if the player clicks on a square that has been already been taken
         // update the user feedback 
         ui.addFeedback("This spot is already taken. Try another one!")
         
@@ -75,16 +94,22 @@ const makeMove = (square) =>{
         // the square the player clicks will place an ( x / o )
         playerSpot.innerText = store.currentGame.turn
 
+        // Store the cell index of the players choice 
+        store.currentGame.storePlayerMove(playerSpot)
+
         // after player chooses a space; switch players
         // and log feedback
         switchPlayer()
         ui.addFeedback(`It is ${store.currentGame.turn}'s turn.`)
 
+        //store player_x move in store.currentGame
+
     }
 
     // log selected square element to the console
     console.log(playerSpot)
-    
+    //check if player move meets win condition
+    checkForMatch()
 }
 
 const switchPlayer =()=>{
@@ -96,11 +121,33 @@ const switchPlayer =()=>{
     }
 }
 
+const checkForMatch = function() {
+    const $topRow = $('.top')
+    const $middleRow = $('.middle')
+    const $bottomRow = $('.bottom')
+    const $leftCol = $('.left')
+    const $rightCol = $('.right')
+    const board = [$topRow,$middleRow,$bottomRow,$leftCol,$rightCol]
+    board.forEach(checkWin)
+    
+}
+
+const checkWin = ($row) => {
+    //if the checked row all contains player classes; then that player wins 
+    if ($row.hasClass(`${store.currentGame.turn}`)){
+        console.log(`${store.currentGame.turn} wins !`)
+    }
+   
+}
+
+
 
 module.exports = {
     onSignUp, 
     Game,
     drawBoard, 
     makeMove, 
-    switchPlayer
+    switchPlayer,
+    checkForMatch,
+    checkWin
 }
